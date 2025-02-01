00afe081c6cd934108655fc681df9a10d2e69779
yantoq123 <rendihariko@gmail.com>
2025-02-01 14:51:40 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="fContainer">
+        <nav class="wraper">
+            <div class="judul">
+                <div class="awal">
+                    <a href="index.php">rendi</a></div>
+                <div class="akhir">
+                    <a href="index.php">gt</a></div>
+            </div>
+            <div>
+                <ul class="list">
+                    <li><a href="" >daftar</a></li>
+                    <li><a href="" class="active">perbayaran</a></li>
+                    <li><a href="">history</a></li>
+                </ul>
+            </div>
+        </nav>
+    </div>
+</body>
+</html>
